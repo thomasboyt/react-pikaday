3856218e4165cf7d1353c19ebf90b53871811d0c
Thomas Boyt <thomas@ledgerx.com>
2015-03-31 12:00:03 -0400

    Pass through `disabled` prop to <input />

--- src/Pikaday.js
+++ src/Pikaday.js
@@ -54,7 +54,7 @@ var ReactPikaday = React.createClass({
   render: function() {
     return (
       <input type="text" ref="pikaday" className={this.props.className}
-        placeholder={this.props.placeholder} />
+        placeholder={this.props.placeholder} disabled={this.props.disabled} />
     );
   }
 });
