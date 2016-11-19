ab9cdd4954c7691a1d8ef4ef4017097510499698
Nick Christensen <nick.g.christensen@gmail.com>
2016-11-19 17:05:28 -0600

    Fix `bundled.js` file path
    `src` => `lib`
    
    Fixes #15

--- bundled.js
+++ bundled.js
@@ -3,6 +3,6 @@
 // this only works in webpack, but it's pretty rad:
 require('pikaday/css/pikaday.css');
 
-var ReactPikaday = require('./src/Pikaday');
+var ReactPikaday = require('./lib/Pikaday');
 
 module.exports = ReactPikaday;
